437a32e9fd797193d6e165308941be6270f7185b
mauri <maur.vegal@duocuc.cl>
2026-06-24 22:58:25 -0400

    Implementación de Swagger y configuración del Gateway

--- Skins/Skins/src/main/java/Skins/controller/SkinController.java
+++ Skins/Skins/src/main/java/Skins/controller/SkinController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/skins")
+@RequestMapping("/skins")
 @RequiredArgsConstructor
 public class SkinController {
     private final SkinService service;
---
+++ gateway-service/src/test/java/com/tienda/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
